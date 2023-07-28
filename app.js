9cef14bffcd8bb79294f72931b2b3e2255c5e083
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-07-28 08:26:12 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+//===============================JS CLASS 1====================================//
+
+
+//==============================Languages=====================================//
+//(1)(Compiler Language)
+//Compiler wo language hoti hy jo sarey programs ko aik sath hi run kara dey jese HTML ho gyi//
+//(2)(Interpreter Language)
+//interpreter wo language hoti hy jo aik aik kr ke programs run karaye jese javascript aik interpreter language hy//
+
+
+//==============================(Variables Types):-=================================//
+//(1)var => we can redeclare it and we can reassign the value in it.
+//(2)let => we cannot redeclare the let but we can reassign the value in let.
+//(3)const => we can not redeclare it nor can change the value of const.
+
+//(Examples):-
+// var name1 = "noor";
+// name1 = "asma";
+// console.log(name1);
+
+//let name2 = "Faha"
+//name2 = "Muqaddas";
+//console.log(name2);
+
+//const name3 = "Maheer";
+//console.log(name3);
+
+
+//=================================objects===============================//
+//multiple data Store kara sktey hain or CRUD operations perform kar sktey hain object main.
+//=========(CRUD Operations)============//
+// C => Create – Add
+// R => Read – View
+// U => Update – Edit
+// D => Delete – Remove
+
+//====(Examples)====//
+//(Objects):-
+//(Here the name,age and city are the keys of object (thisIsObject) and "ayesha","20" and "lahore" are the values of those keys);
+
+// const thisIsObject = {
+// name: "ayesha",
+// age: 20,
+// city: "lahore",
+// }
+
+// C ➔ thisIsObject.CNIC = 123456789
+// R ➔ console.log(thisIsObject);
+// U ➔ thisIsObject.age = 25
+// D ➔ delete thisIsObject.age
+// const keys = Object.keys(thisIsObject)
+// const values = Object.values(thisIsObject)
+// console.log(‘Keys ==➔’, keys);
+// console.log(‘Values ==➔’, values);
+// console.log(‘Obj ==➔’, thisIsObject);
+
+
+
+
+
+
+
